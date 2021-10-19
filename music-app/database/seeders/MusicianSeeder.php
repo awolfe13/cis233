7c3aeec8212e3738e7bba246987f79512c0fc81f
Ariel Wolfe <arielwolfe95@gmail.com>
2021-10-19 11:54:45 -0700

    fixing develop

---
+++ music-app/database/seeders/MusicianSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class MusicianSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Musician::query()->delete();
+
+        $faker = \Faker\Factory::create();
+        
+        foreach(range(1,30) as $number) {
+            \App\Musician::create([
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'instrument' => $faker->randomElement($array = array ('Guitar','Violin','Piano', 'Drums', 'Cello', 'Trumpet', 'Harp', 'Clarinet', 'Flute', 'Bass', 'Saxophone')),
+                'website' => $faker->url,
+            ]);
+        }
+    }
+}
